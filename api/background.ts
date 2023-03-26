b2ca4a19eec30d2cc7b2ec2425df38a87a2a6df8
Amaral Krichman <amaralkrichman@gmail.com>
2023-03-26 19:59:46 -0300

    fix: standardize style

--- api/background.ts
+++ api/background.ts
@@ -1,13 +1,13 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 import { fetchRandomImage } from '../src/lib/unsplash';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   if (req.method === 'GET') {
-    const img = await fetchRandomImage();
+    const data = await fetchRandomImage();
 
-    res.status(200).json(img);
+    res.status(200).json(data);
+  } else {
+    res.status(404);
   }
 }
--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (req: VercelRequest, res: VercelResponse) => {
